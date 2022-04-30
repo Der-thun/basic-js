d00f82d67e85eee80471cf5f48fec77658aad35f
Boris Kortunov <boris.kortunov@gmail.com>
2022-04-30 13:31:43 +0300

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') return false
+  let number = Number(sampleActivity)
+  if (isNaN(number)) return false
+  if (number <= 0) return false
+  let result = Math.ceil(Math.log(15 / number) / (0.693 / 5730))
+  if (result < 0) return false
+  return result
 }
 
 module.exports = {
