189835b82e2ad18c925db4a2d605da4b2261a193
Boris Kortunov <boris.kortunov@gmail.com>
2022-04-30 20:02:56 +0300

    feat: dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function getDNSStats(domains) {
+	let result = {};
+  let fulldom = ''
+  while (domains.length !== 0) {
+  let dom
+  let dotPos = domains[0].lastIndexOf('.')
+  let count = 0
+  if (dotPos !== -1) {dom = domains[0].slice(dotPos)}
+  else {dom = '.'+ domains[0].slice()}
+  for (let i = 0; i < domains.length; i++) {
+  	if (domains[i].includes(dom)) {
+    	count++
+      domains[i] = domains[i].substr(0, domains[i].length - dom.length)
+    }else if(domains[i].includes(dom.slice(1))){
+    	count++
+      domains[i] = ''
+    }
+  }
+  fulldom += dom
+  result[fulldom] = count
+  while(domains.indexOf('') !== -1) {
+  	domains.splice(domains.indexOf(''), 1)
+  }
+  }
+  
+  return result
 }
 
 module.exports = {
