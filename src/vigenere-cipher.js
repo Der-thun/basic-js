c5288ba6637806966925493ef6431c1068015388
Boris Kortunov <boris.kortunov@gmail.com>
2022-05-01 23:06:56 +0300

    feat: vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (typeof message !== 'string' || typeof key !== 'string') throw new Error('Incorrect arguments!')
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let crypMessage = []
+    let crypKey = []
+    let result = ''
+  
+    for (let i = 0; i < key.length; i++) {
+    crypKey.push(alph.indexOf(key[i]))
+    }
+    let keyIndex = 0
+    for (let i = 0; i < message.length; i++) {
+      if(alph.indexOf(message[i]) === -1) {
+        crypMessage.push(message[i])
+        result += message[i]
+      } else {
+        crypMessage.push(alph.indexOf(message[i]))
+        crypMessage[i] += crypKey[keyIndex % crypKey.length]
+        result += alph[crypMessage[i] % alph.length]
+        keyIndex++
+      }	
+    }    
+    return this.direct ? result : result.split('').reverse().join('');
+  }
+
+
+  decrypt(encryptedMessage , key) {
+    if (typeof encryptedMessage !== 'string' || typeof key !== 'string') throw new Error('Incorrect arguments!')
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    let alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let crypMessage = []
+    let crypKey = []
+    let result = ''
+  
+    for (let i = 0; i < key.length; i++) {
+    crypKey.push(alph.indexOf(key[i]))
+    }
+
+   let keyIndex = 0
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if(alph.indexOf(encryptedMessage[i]) === -1) {
+        crypMessage.push(encryptedMessage[i])
+        result += encryptedMessage[i]
+      } else {
+        crypMessage.push(alph.indexOf(encryptedMessage[i]))
+        crypMessage[i] += alph.length - crypKey[keyIndex % crypKey.length]
+        result += alph[crypMessage[i] % alph.length]
+        keyIndex++
+      }    
+    }  
+    return this.direct ? result : result.split('').reverse().join('');
   }
 }
 
